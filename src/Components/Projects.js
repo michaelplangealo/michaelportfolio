4f000b48fb773b652d9385872c22b04b34bc3c00
Michael <Paaekow@Davids-MBP.Butler_Office_320>
2018-06-05 11:28:35 -0500

    animation routes

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { CodeSide } from "../Styled/SideBar";
 import { Intro } from "../Components/About.js";
 import { BodyParty, PageBody, Content } from "../Styled/Pages.js";
+import "./Projects.css";
+import { CSSTransitionGroup } from "react-transition-group";
 
 const PastThings = styled.section`
   height: 20%;
@@ -66,74 +68,83 @@ const ProjDescription = styled.div`
 export default class Projects extends Component {
   render() {
     return (
-      <PageBody>
-        <CodeSide />
-        <BodyParty>
-          <Content>
-            <Intro>These are a few things I've worked on:</Intro>
-            <PastThings>
-              <ProjNames>
-                <a href="manofwar.xyz">MANOFWAR.XYZ</a>
-              </ProjNames>
-              <ProjDescription>
-                <p>
-                  A minimalistic e-commerce website, designed with a
-                  minimalsitic theme. Equipped both login and payment
-                  functionality.
-                </p>
-                <ul>
-                  <li>
-                    Created a sleek, minimalistic design which keeps in mind the
-                    ease of user access.
-                  </li>
-                  <li>Utilizes Auth0 for login functionality</li>
-                  <li>Utilizes Stripe for payment functionality</li>
-                  <li>
-                    Planned updates include order confirmation sent to user's
-                    email via nodemailer, added admin access, and mobile
-                    responsiveness.
-                  </li>
-                </ul>
-              </ProjDescription>
-            </PastThings>
-            <PastThings>
-              <ProjNames>
-                <a href="www.google.com">QODE.CLUB</a>
-              </ProjNames>
-              <ProjDescription>
-                <p>
-                  Qode is a web application aimed at teaching beginners the
-                  basics of html and css.
-                </p>
-                <ul>
-                  <li>
-                    Positioned on the front-end within the team, tasked with
-                    functionality and design.
-                  </li>
-                  <li>
-                    Created structure of the website utilizing Styled Components
-                  </li>
-                  <li>
-                    Rendered proper information to their matching components
-                  </li>
-                  <li>Developed page routes and re-directs</li>
-                </ul>
-              </ProjDescription>
-            </PastThings>
-            <PastThings>
-              <ProjNames>
-                <a>MICHAELPLANGE.COM</a>
-              </ProjNames>
-              <ProjDescription>
-                <p>The website you're looking at right now.</p>
-                <ul>
-                  <li>Kind of like a "the brain named itself" situation.</li>
-                </ul>
-              </ProjDescription>
-            </PastThings>
-          </Content>
-        </BodyParty>
-      </PageBody>
+      <CSSTransitionGroup
+        transitionName="homeTransition"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnter={false}
+        transitionLeave={false}
+      >
+        <PageBody>
+          <CodeSide />
+          <BodyParty>
+            <Content>
+              <Intro>These are a few things I've worked on:</Intro>
+              <PastThings>
+                <ProjNames>
+                  <a href="manofwar.xyz">MANOFWAR.XYZ</a>
+                </ProjNames>
+                <ProjDescription>
+                  <p>
+                    A minimalistic e-commerce website, designed with a
+                    minimalsitic theme. Equipped both login and payment
+                    functionality.
+                  </p>
+                  <ul>
+                    <li>
+                      Created a sleek, minimalistic design which keeps in mind
+                      the ease of user access.
+                    </li>
+                    <li>Utilizes Auth0 for login functionality</li>
+                    <li>Utilizes Stripe for payment functionality</li>
+                    <li>
+                      Planned updates include order confirmation sent to user's
+                      email via nodemailer, added admin access, and mobile
+                      responsiveness.
+                    </li>
+                  </ul>
+                </ProjDescription>
+              </PastThings>
+              <PastThings>
+                <ProjNames>
+                  <a href="www.google.com">QODE.CLUB</a>
+                </ProjNames>
+                <ProjDescription>
+                  <p>
+                    Qode is a web application aimed at teaching beginners the
+                    basics of html and css.
+                  </p>
+                  <ul>
+                    <li>
+                      Positioned on the front-end within the team, tasked with
+                      functionality and design.
+                    </li>
+                    <li>
+                      Created structure of the website utilizing Styled
+                      Components
+                    </li>
+                    <li>
+                      Rendered proper information to their matching components
+                    </li>
+                    <li>Developed page routes and re-directs</li>
+                  </ul>
+                </ProjDescription>
+              </PastThings>
+              <PastThings>
+                <ProjNames>
+                  <a>MICHAELPLANGE.COM</a>
+                </ProjNames>
+                <ProjDescription>
+                  <p>The website you're looking at right now.</p>
+                  <ul>
+                    <li>Kind of like a "the brain named itself" situation.</li>
+                  </ul>
+                </ProjDescription>
+              </PastThings>
+            </Content>
+          </BodyParty>
+        </PageBody>
+      </CSSTransitionGroup>
     );
   }
 }
--- src/Components/Write.js
+++ src/Components/Write.js
@@ -2,16 +2,26 @@ import styled from "styled-components";
 import React, { Component } from "react";
 import { WriteSide } from "../Styled/SideBar.js";
 import { BodyParty, PageBody, Content } from "../Styled/Pages.js";
+import "./Write.css";
+import { CSSTransitionGroup } from "react-transition-group";
 
 export default class Write extends Component {
   render() {
     return (
-      <PageBody>
-        <WriteSide />
-        <BodyParty>
-          <Content>coming soon</Content>
-        </BodyParty>
-      </PageBody>
+      <CSSTransitionGroup
+        transitionName="homeTransition"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnter={false}
+        transitionLeave={false}
+      >
+        <PageBody>
+          <WriteSide />
+          <BodyParty>
+            <Content>coming soon</Content>
+          </BodyParty>
+        </PageBody>
+      </CSSTransitionGroup>
     );
   }
 }
